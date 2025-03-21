0a7dbbe9c6effbe68d85c43bcdba996b9989d628
Egor Petruschenko <jaguar.94@mail.ru>
2025-03-21 14:12:39 +0600

    исправление Dockerfile

--- config/settings.py
+++ config/settings.py
@@ -94,6 +94,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
